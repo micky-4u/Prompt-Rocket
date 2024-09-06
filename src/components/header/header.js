460f4ebb1a26a0e71a1e97566d246b4255e92c11
micky-4u <laryeamichael4u@gmail.com>
2024-09-06 13:10:50 +0000

    header feature

---
+++ src/components/header/header.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./header.css";
+const Header = () =>{
+
+    return(
+        <div className="nav">
+            <div className="nav-logo">
+                <img src="" alt="logo"/>
+            </div>
+
+            <ul>
+                <li>Home</li>
+                <li>Community</li>
+                <li>About</li>
+                <li>Blog</li>
+            </ul>
+
+            <div  className="sign-up">
+                <button>Sign up</button>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
---
+++ src/pages/home.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "../assets/css/index.css"
+const Home = ()=>{
+
+    return(
+        <div className="home">
+            <div className="home-content">
+                <div className="left"></div>
+                <div className="mid">
+                    <h1>Enjoy the power and more</h1>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.N
+                        eque magnam mollitia architecto delectus voluptatem, nam, 
+                        praesentium voluptatum id est ex officia laboriosam consectetur 
+                        molestias reiciendis unde minus, vero maxime aspernatur!
+
+                    </p>
+                    <div>
+                        <button>Right</button>
+                        <button>Left</button>
+                    </div>
+                </div>
+                <div className="right"></div>
+
+            </div>
+        </div>
+    )
+}
+
+export default Home;
